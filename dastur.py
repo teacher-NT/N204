431224e03ba61221719e230d64d29677bd8dc3cd
Teacher NT <holmonbek@gmail.com>
2026-05-08 14:45:58 +0500

    Tuple

--- dastur.py
+++ dastur.py
@@ -1,8 +1,17 @@
 import os
 os.system("cls")
 
-tuple1 = ("Salom", "Hello", 1,2,3)
+# tuple1 = ('Salom', "Hello", 1,2,3)
 
-# tuple1[2] = "Nice"
-print(tuple1)
-print(tuple1[3])
+# # tuple1[2] = "Nice"
+# print(tuple1)
+# print(tuple1[3])
+
+tuple1 = (1,2,4,0,3,8,5,7,6,9)
+# print(tuple1[15])
+print(tuple1[3:])
+print(tuple1[:5])
+print(tuple1[2:6])
+print(tuple1[1:8:2])
+print(tuple1[-2]) # orqadan keladi
+print(tuple1[::-1])
