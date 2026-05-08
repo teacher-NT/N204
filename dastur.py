1da4017f0058b7a3d389ecc091d0245d3b2338f7
Teacher NT <holmonbek@gmail.com>
2026-05-08 14:53:41 +0500

    If in tuple

--- dastur.py
+++ dastur.py
@@ -7,11 +7,18 @@ os.system("cls")
 # print(tuple1)
 # print(tuple1[3])
 
-tuple1 = (1,2,4,0,3,8,5,7,6,9)
-# print(tuple1[15])
-print(tuple1[3:])
-print(tuple1[:5])
-print(tuple1[2:6])
-print(tuple1[1:8:2])
-print(tuple1[-2]) # orqadan keladi
-print(tuple1[::-1])
+# tuple1 = (1,2,4,0,3,8,5,7,6,9)
+# # print(tuple1[15])
+# print(tuple1[3:])
+# print(tuple1[:5])
+# print(tuple1[2:6])
+# print(tuple1[1:8:2])
+# print(tuple1[-2]) # orqadan keladi
+# print(tuple1[::-1])
+
+
+fruits = ("apple",'banana','peach','melon','kivi')
+if 'banana' in fruits:
+    print("Yes")
+else:
+    print("No")
