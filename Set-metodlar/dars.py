21e12ee95df1950e5cd2fbcf5b4c38900cf393a0
Teacher NT <holmonbek@gmail.com>
2026-05-11 15:12:52 +0500

    Set methods

---
+++ Set-metodlar/dars.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+# set1 = {"Salom", "Hayr", 1,2,34,52,1,2, "Salom"}
+# # set1[2] = 'hello'
+# print(set1)
+
+# set2 = {45,65,12,90,8,51,5}
+# print(set2)
+
+
+set3 = {"olma", 'banan', 'gilos', 'mandarin'}
+# if 'Olma' in set3:
+#     print("Yes")
+# else:
+#     print("No")
+
+for i in set3:
+    print(i.upper(), end=" ")
---
+++ Set-metodlar/main.py
@@ -0,0 +1,47 @@
+import os
+os.system("cls")
+
+# set1 = {21,122,224,35,26,9}
+
+# set1.add(3)
+# set1.add(4)
+# print(set1)
+
+
+# set1 = {21,122,224,35,26,9}
+# set1.remove(21)
+# print(set1)
+
+# set1 = {21,122,224,35,26,9}
+# set1.discard(22)
+# print(set1)
+
+# set1 = {21,122,224,35,26,9}
+# n = set1.pop()
+# print(set1, n)
+# n = set1.pop()
+# print(set1, n)
+
+
+# set1 = {21,122,224,35,26,9}
+# set1.clear()
+# print(set1)
+
+
+# set1 = {21,122,224,35,26,9}
+# set2 = set1.copy()
+# set2.add(12)
+# print(set1)
+# print(set2)
+
+
+# set1 = {1,2,3,4,5,6,7}
+# set2 = {4,5,6,7,8,9}
+# set1.update(set2)
+# print(set1)
+
+set1 = {1,2,3,4,5,6}
+set2 = {4,5,6,7,8,9}
+set3 = set1.union(set2)
+print(set1)
+print(set3)
