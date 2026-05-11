dffc1e84211d28adda360acbe8231c11f6c4dc2f
Teacher NT <holmonbek@gmail.com>
2026-05-11 15:29:55 +0500

    Set methods

--- Set-metodlar/main.py
+++ Set-metodlar/main.py
@@ -40,8 +40,45 @@ os.system("cls")
 # set1.update(set2)
 # print(set1)
 
-set1 = {1,2,3,4,5,6}
-set2 = {4,5,6,7,8,9}
-set3 = set1.union(set2)
-print(set1)
-print(set3)
+# set1 = {1,2,3,4,5,6}
+# set2 = {4,5,6,7,8,9}
+# set3 = set1.union(set2)
+# print(set1)
+# print(set3)
+
+
+# ===================================================================
+
+
+# set1 = {1,2,3,4,5,6}
+# set2 = {4,5,6,7,8,9}
+
+# set3 = set1.intersection(set2)
+# print(set3)
+# set1.intersection_update(set2)
+# print(set1)
+
+
+# set1 = {1,2,3,4,5,6}
+# set2 = {4,5,6,7,8,9}
+
+# set3 = set1.difference(set2)
+# print(set3)
+# set1.difference_update(set2)
+# print(set1)
+
+
+# set1 = {1,2,3,4,5,6}
+# set2 = {4,5,6,7,8,9}
+
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
+
+# set1.symmetric_difference_update(set2)
+# print(set1)
+
+
+# set1 = {1,2,3,4,5,6}
+# set2 = {1,2,4,5,6,7,8,9,3}
+# # print(set1.issubset(set2))
+# print(set2.issuperset(set1))
