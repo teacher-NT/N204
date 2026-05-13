bc541ed748e78614a6dea1594311e3ec77dd793b
Teacher NT <holmonbek@gmail.com>
2026-05-13 14:24:43 +0500

    Dict 1

--- Set-metodlar/main.py
+++ Set-metodlar/main.py
@@ -72,6 +72,7 @@ os.system("cls")
 # set2 = {4,5,6,7,8,9}
 
 # set3 = set1.symmetric_difference(set2)
+# set3 = set1 ^ set2
 # print(set3)
 
 # set1.symmetric_difference_update(set2)
---
+++ dict-metodlar/1.py
@@ -0,0 +1,16 @@
+
+import os
+os.system("cls")
+
+car = {
+    "brand": "BMW",
+    "model": "M5 F90 Competition",
+    "price": 128000,
+    "color": "green",
+    "year": 2023
+}
+car['country'] = "Germany"
+
+print(car)
+car["brand"] = 'Zeekr'
+print(car["brand"])
