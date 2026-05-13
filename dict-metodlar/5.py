77ce59098dbc7073d5445103d9f00ea719cab20f
Teacher NT <holmonbek@gmail.com>
2026-05-13 15:16:14 +0500

    dict 5

---
+++ dict-metodlar/5.py
@@ -0,0 +1,39 @@
+import os
+os.system("cls")
+
+car = {
+    "brand": "BMW",
+    "model": "M5 F90 Competition",
+    "price": 128000,
+    "color": "GREEN",
+    "year": 2023
+}
+
+# print(car['mode'])
+# print(car.get('model', 'Kalit topilmadi!'))
+
+# print(list(car.keys()))
+# print(list(car.values()))
+
+# n = car.pop('color')
+# print(n)
+# print(car)
+
+# n = car.popitem()
+# print(n)
+# print(car)
+
+
+# print(car.items())
+# for k,q in car.items():
+#     print(k, q)
+
+# for i in car:
+#     print(i, car[i])
+
+
+# car['price'] = 95000
+# car['model'] = 'M8'
+# car['color'] = 'Blue'
+# car.update({"price": 95000,'model':'M8', 'color':'Blue'})
+# print(car)
