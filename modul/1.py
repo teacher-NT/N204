43cfb61936fa94c953b6fec80f7f38fa95fecd31
Teacher NT <holmonbek@gmail.com>
2026-05-20 15:09:17 +0500

    math modul

---
+++ modul/1.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.pow(3,4))
+
+print(math.sqrt(25))
+
+print(math.log2(8))
+print(math.log(64, 8))
+
+print(math.floor(4.9))
+print(math.ceil(4.2))
+print(round(4.2))
+print(round(4.7))
+print(round(4.223456789, 1))
