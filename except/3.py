a7ea2b268327ec6b87120248bc871e7e9647c2c1
Teacher NT <holmonbek@gmail.com>
2026-05-20 14:42:53 +0500

    finally

---
+++ except/3.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+try:
+    a = int(input("Son kiriting: "))
+    b = int(input("Son kiriting: "))
+    print(a+b)
+except:
+    print("Xatolik sodir bo'ldi!")
+else:
+    print("Try muvafaqqiyatli bajarildi!")
+finally:
+    print("Exceptiondan chiqdik!")
