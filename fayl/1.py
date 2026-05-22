777104dc7573d2087deae562ed241835df75e09b
Teacher NT <holmonbek@gmail.com>
2026-05-22 14:21:35 +0500

    file 1.2

--- fayl/1.py
+++ fayl/1.py
@@ -1,5 +1,9 @@
 
-file = open("N204/fayl/test.txt")
-matn = file.read()
-print(matn)
-file.close()
+# file = open("N204/fayl/test.txt")
+# matn = file.read()
+# print(matn)
+# file.close()
+
+with open("N204/fayl/test.txt") as file:
+    matn = file.read()
+    print(matn)
