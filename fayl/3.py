28e6b67097389cddc1eaa72280230d1c33279f16
Teacher NT <holmonbek@gmail.com>
2026-05-22 14:35:39 +0500

    file 3

---
+++ fayl/3.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+# with open("N204/fayl/test.txt") as file:
+#     for i in file:
+#         print(i)
+
+
+# with open("N204/fayl/test2.txt", "w") as file:
+#     file.write("Abdullo bugun darsga o'z vaqtida keldi")
+
+
+with open("N204/fayl/test2.txt", "a") as file:
+    file.write("\nAbdullo bugun darsga o'z vaqtida keldi")
