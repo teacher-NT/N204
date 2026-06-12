fdd1a44c875dd24e1d1578479f1e9c74284d1807
Teacher NT <holmonbek@gmail.com>
2026-06-12 14:43:00 +0500

    dunder methods 1

---
+++ OOP/9.py
@@ -0,0 +1,28 @@
+import os
+os.system("cls")
+
+
+class School:
+    def __init__(self, m, n, r, t):
+        self.manzil = m
+        self.soni = n
+        self.raqam = r
+        self.til = t
+    
+    def __str__(self):
+        return f"{self.manzil} {self.soni} {self.raqam} {self.til}"
+    
+    def __gt__(self, n):
+        print("Katta operatori")
+        return self.soni > n
+    
+    def __lt__(self, n):
+        return self.soni < n
+
+    def __eq__(self, n):
+        return self.soni == n
+    
+s1 = School("Chilonzor", 2000, 78, "O'zbek tili")
+print(s1 > 5000)
+print(s1 < 500, s1.soni < 500)
+print(s1 == 2000)
